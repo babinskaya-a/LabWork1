40e9af34b8f7e62880cee550fd0ce2b335836197
babinskaya-a <st139880@student.spbu.ru>
2025-12-11 11:50:35 +0000

    Fixing image color

--- BmpImage.cpp
+++ BmpImage.cpp
@@ -110,11 +110,11 @@ std::unique_ptr<Image> BmpImage::rotateCW() const {
 	for (uint32_t y = 0; y < oldH; y++) {
 		for (uint32_t x = 0; x < oldW; x++) {
 			uint8_t r, g, b;
-			getPixel(x, y, b, g, r);
+			getPixel(x, y, r, g, b);
 
 			uint32_t newX = oldH - 1 - y;
 			uint32_t newY = x;
-			newImg->setPixel(newX, newY, b, g, r);
+			newImg->setPixel(newX, newY, r, g, b);
 		}
 	}
 	return newImg;
